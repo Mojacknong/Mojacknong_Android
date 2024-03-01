857ed218d117275bced4c40359a2583b6612f6d9
arinming <jyandysys1@naver.com>
2024-03-01 20:43:07 +0900

    [Add] 팜어스 텍스트 스타일 테마 추가 (#119)

---
+++ lib/common/farmus_theme_text_style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+import 'farmus_theme_color.dart';
+
+class FarmusThemeTextStyle {
+
+  // dark
+  static const TextStyle darkSemiBold20 = TextStyle(
+    color: FarmusThemeColor.dark,
+    fontSize: 20,
+    fontWeight: FontWeight.w600
+  );
+
+  // green1
+  static const TextStyle green1Medium14 = TextStyle(
+      color: FarmusThemeColor.green1,
+      fontSize: 14,
+  );
+}
--- lib/view/on_boarding/on_boarding_screen.dart
+++ lib/view/on_boarding/on_boarding_screen.dart
@@ -1,6 +1,7 @@
+import 'package:farmus/common/farmus_theme_color.dart';
+import 'package:farmus/common/farmus_theme_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:mojacknong_android/common/farmus_theme_color.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -15,16 +16,27 @@ class OnBoardingScreen extends StatelessWidget {
         actions: const [
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text("1/4", style: TextStyle(color: FarmusThemeColor.green1),),
+            child: Text(
+              "1/4",
+              style: FarmusThemeTextStyle.green1Medium14,
+            ),
           )
         ],
         centerTitle: true,
       ),
       body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Divider(
             thickness: 1,
             color: FarmusThemeColor.grey4,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "반가워요!\n프로필을 설정해볼까요?",
+              style: FarmusThemeTextStyle.darkSemiBold20,
+            ),
           )
         ],
       ),
