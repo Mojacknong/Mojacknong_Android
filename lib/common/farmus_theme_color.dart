8c8a41dc0fcfb8ff455761693dbca12193c0f715
arinming <jyandysys1@naver.com>
2024-03-01 20:13:54 +0900

    [Add] 팜어스 컬러 테마 추가 (#119)

---
+++ lib/common/farmus_theme_color.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class FarmusThemeColor {
+  static const Color dark = Color(0xFF2B2B2B);
+
+  static const Color grey1 = Color(0xFF505050);
+  static const Color grey2 = Color(0xFF7D7D7D);
+  static const Color grey3 = Color(0xFFC5C5C5);
+  static const Color grey4 = Color(0xFFECECEC);
+  static const Color grey5 = Color(0xFFF2F2F2);
+
+  static const Color background = Color(0xFFF7F7F7);
+  static const Color primary = Color(0xFF184B36);
+
+  static const Color green = Color(0xFFBDF69A);
+  static const Color green1 = Color(0xFF24AD5D);
+  static const Color green2 = Color(0xFF2CC167);
+  static const Color greenLight = Color(0xFFEFFEEE);
+  static const Color greenLight2 = Color(0xFFDAF3EB);
+
+  static const Color brownText = Color(0xFFA28B76);
+  static const Color brownButton = Color(0xFF998669);
+  static const Color brown2 = Color(0xFFEBD6AF);
+  static const Color brown3 = Color(0xFFF6F1EB);
+
+  static const Color red = Color(0xFFFF4040);
+
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
--- lib/view/on_boarding/on_boarding_screen.dart
+++ lib/view/on_boarding/on_boarding_screen.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mojacknong_android/common/farmus_theme_color.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -9,15 +9,24 @@ class OnBoardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: SvgPicture.asset("assets/image/ic_close.svg")),
-        actions: const [Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text("1/4"),
-        )],
+        leading: IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/image/ic_close.svg")),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text("1/4", style: TextStyle(color: FarmusThemeColor.green1),),
+          )
+        ],
         centerTitle: true,
       ),
       body: const Column(
-        children: [],
+        children: [
+          Divider(
+            thickness: 1,
+            color: FarmusThemeColor.grey4,
+          )
+        ],
       ),
     );
   }
