86b85c4365ba887a8505c86e06f96d829ac85895
arinming <jyandysys1@naver.com>
2024-03-17 17:04:42 +0900

    [Feat] 온보딩 버튼 통일화 (#129)

--- lib/view/home/home_vegi_add_screen.dart
+++ lib/view/home/home_vegi_add_screen.dart
@@ -1,4 +1,5 @@
 import 'package:farmus/common/app_bar/primary_app_bar.dart';
+import 'package:farmus/view/home/component/home_add_sub_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
@@ -19,7 +20,15 @@ class HomeVegiAddScreen extends ConsumerWidget {
         title: "채소 등록",
         centerTitle: false,
       ),
-      body: Container(),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [HomeAddSubTitle(text: "등록할 채소를 선택해주세요")],
+          ),
+        ),
+      ),
     );
   }
 }
