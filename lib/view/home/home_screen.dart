25b5ed76e1d290b68204c62dffddfe66b0654b11
arinming <jyandysys1@naver.com>
2024-03-23 01:25:32 +0900

    [Feat] 홈 화면에서 채소 없을 때의 화면 분리 (#131)

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,11 +1,7 @@
+import 'package:farmus/view/home/component/home_none_vegi.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/app_bar/home_app_bar.dart';
-import 'component/home_motivation.dart';
-import 'component/home_my_vegi.dart';
-import 'component/home_none_content.dart';
-import 'component/home_sub_title.dart';
-import 'component/home_to_do.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,26 +13,7 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              HomeMyVegi(),
-              HomeMotivation(
-                motivation: '텃밭에서 식탁까지 팜어스와 늘 함께해요!',
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              HomeSubTitle(title: "오늘 할 일"),
-              HomeToDo(),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: HomeNoneContent(text: "아직 루틴을 등록하지 않았어요"),
-              ),
-              HomeSubTitle(title: "성장 일기"),
-              HomeNoneContent(text: "아직 작성한 일기가 없어요"),
-            ],
-          ),
+          child: HomeNoneVegi(),
         ),
       ),
     );
