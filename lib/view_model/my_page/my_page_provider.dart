e04652e2b165c75008025257ac97358d38787e7c
geonheey <ghyjas0704@gmail.com>
2024-03-25 18:36:09 +0900

    [Feat] 팜클럽 및 나의 인증 구현 (#123)

--- lib/view_model/my_page/my_page_provider.dart
+++ lib/view_model/my_page/my_page_provider.dart
@@ -1,4 +1,5 @@
 import 'package:farmus/view_model/my_page/notifier/%08page_notifier.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final myPageProvider = StateNotifierProvider<PageNotifier, String>((ref) {
