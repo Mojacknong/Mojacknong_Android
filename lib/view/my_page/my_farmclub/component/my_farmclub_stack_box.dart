211a0594e603f30ef5fa42ecfe14296ee50da3ba
geonheey <ghyjas0704@gmail.com>
2024-03-31 16:11:11 +0900

    [Refactor] 폰트 통일 및 화면이동방법 수정 (#138)

---
+++ lib/view/my_page/my_farmclub/component/my_farmclub_stack_box.dart
@@ -0,0 +1,89 @@
+import 'package:farmus/common/theme/farmus_theme_text_style.dart';
+import 'package:farmus/view/my_page/my_farmclub/component/farmclub_widget.dart';
+import 'package:farmus/view/my_page/my_farmclub/component/my_farmclub_box.dart';
+import 'package:farmus/view_model/my_page/vege_count_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyFarmClubStackBox extends ConsumerWidget {
+  final String historyType;
+  final Image? image;
+  final String message;
+  final String? imagePath = 'assets/image/ic_farmclub.png';
+  final String? vegeImage;
+
+  const MyFarmClubStackBox({
+    Key? key,
+    required this.historyType,
+    this.image,
+    this.vegeImage,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final vegeCount = ref.watch(vegeCountProvider);
+
+    return Stack(
+      children: [
+        const MyFarmClubBox(),
+        Padding(
+          padding: const EdgeInsets.only(left: 32, top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (image != null) ...[
+                    Image.asset(
+                      imagePath!,
+                      height: 22,
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                            text: historyType,
+                            style: FarmusThemeTextStyle.darkSemiBold17),
+                        TextSpan(
+                            text: ' ${vegeCount.toString()}',
+                            style: FarmusThemeTextStyle.gray2SemiBold17),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                        text: ' ${vegeCount.toString()} ',
+                        style: FarmusThemeTextStyle.green1SemiBold13),
+                    TextSpan(
+                        text: message,
+                        style: FarmusThemeTextStyle.gray1SemiBold13),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Positioned(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 90.0, left: 8),
+                child: FarmClubWidget(),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
