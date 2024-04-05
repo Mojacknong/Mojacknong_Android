ad280751865bfd0c512f5bf2ef5af55be669fa71
arinming <jyandysys1@naver.com>
2024-04-06 02:04:55 +0900

    [Feat] 화면 터치시 키보드 내리기 추가 (#137)

--- lib/view/vegi_add/home_vegi_add_screen.dart
+++ lib/view/vegi_add/home_vegi_add_screen.dart
@@ -55,116 +55,122 @@ class HomeVegiAddScreen extends ConsumerWidget {
       }
     }
 
-    return Scaffold(
-      appBar: const BackLeftTitleAppBar(
-        title: "채소 등록",
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Consumer(builder: (context, ref, _) {
-              switch (currentPageIndex) {
-                case "first":
-                  return const HomeVegiAddFirst();
-                case "second":
-                  return const HomeVegiAddSecond();
-                default:
-                  return Container();
-              }
-            }),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Visibility(
-                    visible: currentPageIndex != "first",
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: const BackLeftTitleAppBar(
+          title: "채소 등록",
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Consumer(builder: (context, ref, _) {
+                switch (currentPageIndex) {
+                  case "first":
+                    return const HomeVegiAddFirst();
+                  case "second":
+                    return const HomeVegiAddSecond();
+                  default:
+                    return Container();
+                }
+              }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Visibility(
+                      visible: currentPageIndex != "first",
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: PrimaryButton(
+                          width: double.infinity,
+                          height: 48,
+                          text: "이전",
+                          onPressed: () {
+                            switch (currentPageIndex) {
+                              case "first":
+                                return;
+                              case "second":
+                                movePage.moveToFirstPage();
+                            }
+                          },
+                          enabled: true,
+                          textColor: FarmusThemeColor.gray1,
+                          backgroundColor: FarmusThemeColor.white,
+                          borderColor: FarmusThemeColor.gray3,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: PrimaryButton(
                         width: double.infinity,
                         height: 48,
-                        text: "이전",
+                        text: currentPageIndex == "first" ? "다음" : "완료",
                         onPressed: () {
                           switch (currentPageIndex) {
                             case "first":
-                              return;
+                              movePage.moveToSecondPage();
                             case "second":
-                              movePage.moveToFirstPage();
+                              Navigator.pop(context);
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    Future.delayed(const Duration(seconds: 2),
+                                        () {
+                                      Navigator.of(context).pop();
+                                    });
+                                    return PrimaryDialog(
+                                      title: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            SvgPicture.asset(
+                                                "assets/image/ic_check.svg"),
+                                            const SizedBox(
+                                              width: 8,
+                                            ),
+                                            const Text(
+                                              "새 채소가 등록되었어요",
+                                              style: FarmusThemeTextStyle
+                                                  .darkMedium16,
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  });
                           }
                         },
-                        enabled: true,
-                        textColor: FarmusThemeColor.gray1,
-                        backgroundColor: FarmusThemeColor.white,
-                        borderColor: FarmusThemeColor.gray3,
+                        enabled: currentPageIndex == "first"
+                            ? isVegiSelectedComplete
+                            : isVegiAddInfoComplete,
+                        textColor: getButtonTextColor(currentPageIndex,
+                            isVegiSelectedComplete, isVegiAddInfoComplete),
+                        backgroundColor: getButtonBackgroundColor(
+                            currentPageIndex,
+                            isVegiSelectedComplete,
+                            isVegiAddInfoComplete),
+                        borderColor: FarmusThemeColor.white,
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: PrimaryButton(
-                      width: double.infinity,
-                      height: 48,
-                      text: currentPageIndex == "first" ? "다음" : "완료",
-                      onPressed: () {
-                        switch (currentPageIndex) {
-                          case "first":
-                            movePage.moveToSecondPage();
-                          case "second":
-                            Navigator.pop(context);
-                            showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  Future.delayed(const Duration(seconds: 2),
-                                      () {
-                                    Navigator.of(context).pop();
-                                  });
-                                  return PrimaryDialog(
-                                    title: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          SvgPicture.asset(
-                                              "assets/image/ic_check.svg"),
-                                          const SizedBox(
-                                            width: 8,
-                                          ),
-                                          const Text(
-                                            "새 채소가 등록되었어요",
-                                            style: FarmusThemeTextStyle
-                                                .darkMedium16,
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  );
-                                });
-                        }
-                      },
-                      enabled: currentPageIndex == "first"
-                          ? isVegiSelectedComplete
-                          : isVegiAddInfoComplete,
-                      textColor: getButtonTextColor(currentPageIndex,
-                          isVegiSelectedComplete, isVegiAddInfoComplete),
-                      backgroundColor: getButtonBackgroundColor(
-                          currentPageIndex,
-                          isVegiSelectedComplete,
-                          isVegiAddInfoComplete),
-                      borderColor: FarmusThemeColor.white,
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
