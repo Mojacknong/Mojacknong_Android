87c27b371d0b43bbea66b7465a6ea537989e9fb5
arinming <jyandysys1@naver.com>
2024-04-08 20:56:48 +0900

    [Feat] 성장 일기 네비게이션 구현 (#143)

--- lib/view/home/component/home_vege_diary.dart
+++ lib/view/home/component/home_vege_diary.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:farmus/view/vege_diary/vege_diary_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -12,44 +13,52 @@ class HomeVegeDiary extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Row(
-      children: [
-        Container(
-          width: 64,
-          height: 64,
-          decoration: ShapeDecoration(
-            color: FarmusThemeColor.background,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const VegeDiaryScreen()),
+        );
+      },
+      child: Row(
+        children: [
+          Container(
+            width: 64,
+            height: 64,
+            decoration: ShapeDecoration(
+              color: FarmusThemeColor.background,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            child: SvgPicture.asset(
+              "assets/image/ic_farm_club_mark.svg",
+              fit: BoxFit.cover,
             ),
           ),
-          child: SvgPicture.asset(
-            "assets/image/ic_farm_club_mark.svg",
-            fit: BoxFit.cover,
+          const SizedBox(
+            width: 16,
           ),
-        ),
-        const SizedBox(
-          width: 16,
-        ),
-        const Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "2023년 11월 25일",
-                style: FarmusThemeTextStyle.gray1Medium13,
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Text(
-                "우리 상훈이가 쑥쑥 자라고 있네? 얼른 다 자라서 삼겹살이랑 쌈장 마늘 해서···",
-                style: FarmusThemeTextStyle.darkMedium15,
-              ),
-            ],
-          ),
-        )
-      ],
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "2023년 11월 25일",
+                  style: FarmusThemeTextStyle.gray1Medium13,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "우리 상훈이가 쑥쑥 자라고 있네? 얼른 다 자라서 삼겹살이랑 쌈장 마늘 해서···",
+                  style: FarmusThemeTextStyle.darkMedium15,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
