f6a2b70e7073b4aece2185d467d72a4463f718b2
arinming <jyandysys1@naver.com>
2024-04-15 03:42:35 +0900

    [Feat] 팜어스 스위치 구현 (#143)

---
+++ lib/view/vege_diary_write/component/vege_diary_write_switch.dart
@@ -0,0 +1,37 @@
+import 'package:farmus/common/theme/farmus_theme_color.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class VegeDiaryWriteSwitch extends ConsumerStatefulWidget {
+  const VegeDiaryWriteSwitch({super.key});
+
+  @override
+  ConsumerState createState() => _VegeDiaryWriteSwitchState();
+}
+
+class _VegeDiaryWriteSwitchState extends ConsumerState<VegeDiaryWriteSwitch> {
+  bool switchValue = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        useMaterial3: true,
+      ).copyWith(
+        colorScheme: Theme.of(context).colorScheme.copyWith(outline: FarmusThemeColor.gray3)
+      ),
+      child: Switch(
+          value: switchValue,
+          activeColor: FarmusThemeColor.white,
+          activeTrackColor: FarmusThemeColor.primary,
+          inactiveThumbColor: FarmusThemeColor.white,
+          inactiveTrackColor: FarmusThemeColor.gray3,
+          onChanged: (bool? value) {
+            setState(() {
+              switchValue = value ?? false;
+            });
+          }),
+    );
+  }
+}
