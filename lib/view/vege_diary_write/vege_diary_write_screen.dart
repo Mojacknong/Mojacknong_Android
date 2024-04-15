09c9abbc1e31921a1e746e51cf172cb93d6e80e9
arinming <jyandysys1@naver.com>
2024-04-15 21:30:29 +0900

    [Feat] 일기 쓰기 UI 구현 (#145)

--- lib/view/vege_diary_write/vege_diary_write_screen.dart
+++ lib/view/vege_diary_write/vege_diary_write_screen.dart
@@ -1,12 +1,15 @@
 import 'package:farmus/common/app_bar/primary_app_bar.dart';
 import 'package:farmus/common/button/primary_button.dart';
+import 'package:farmus/common/image_picker/primary_image_picker.dart';
 import 'package:farmus/view/vege_diary_write/component/vege_diary_write_bottom.dart';
 import 'package:farmus/view/vege_diary_write/component/vege_diary_write_state.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../common/theme/farmus_theme_color.dart';
+import '../vege_delete/component/content_input_text_form.dart';
 
 class VegeDiaryWriteScreen extends ConsumerWidget {
   const VegeDiaryWriteScreen({super.key});
@@ -47,19 +50,36 @@ class VegeDiaryWriteScreen extends ConsumerWidget {
       body: const Column(
         children: [
           Expanded(
-            child: Column(
-              children: [],
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 48.0,
+                      vertical: 8.0,
+                    ),
+                    child: PrimaryImagePicker(),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ContentInputTextForm(
+                      maxLength: 300,
+                    ),
+                  ),
+                  VegeDiaryWriteState(),
+                ],
+              ),
             ),
           ),
           Column(
             children: [
-              VegeDiaryWriteState(),
-              SizedBox(
-                height: 16.0,
-              ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16.0),
                 child: Divider(
+                  height: 1,
                   thickness: 1,
                   color: FarmusThemeColor.gray4,
                 ),
