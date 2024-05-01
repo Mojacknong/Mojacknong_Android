32acc87932b57c00cc288c959753d1a41223a061
arinming <jyandysys1@naver.com>
2024-05-01 18:51:10 +0900

    [Feat] 일기 쓰기 완료 버튼 클릭시 창 닫기 (#145)

--- lib/view_model/vege_diary_write/vege_diary_write_provider.dart
+++ lib/view_model/vege_diary_write/vege_diary_write_provider.dart
@@ -2,6 +2,7 @@ import 'package:farmus/model/my_vege/vege_diary_write_model.dart';
 import 'package:farmus/view_model/vege_diary_write/notifier/vege_diary_write_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final vegeDiaryWriteProvider = StateNotifierProvider<VegeDiaryWriteNotifier, VegeDiaryWriteModel>((ref) {
+final vegeDiaryWriteProvider =
+    StateNotifierProvider<VegeDiaryWriteNotifier, VegeDiaryWriteModel>((ref) {
   return VegeDiaryWriteNotifier();
 });
