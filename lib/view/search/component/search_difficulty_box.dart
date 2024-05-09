bb11a123905f628343c15eef5f6d3ab375a8dd00
geonheey <ghyjas0704@gmail.com>
2024-05-09 10:28:42 +0900

    [Feat] box 크기 padding으로 수정 (#148)

--- lib/view/search/component/search_difficulty_box.dart
+++ lib/view/search/component/search_difficulty_box.dart
@@ -10,58 +10,55 @@ class SearchDifficultyBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
+      child: Container(
+        width: double.infinity,
+        height: 63,
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(width: 1, color: FarmusThemeColor.gray4),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: Positioned(
+          left: 16,
+          top: 16,
+          child: Container(
             padding: const EdgeInsets.only(right: 16),
-            height: 63,
-            decoration: ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(width: 1, color: FarmusThemeColor.gray4),
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-            child: Positioned(
-              left: 16,
-              top: 16,
-              child: SizedBox(
-                width: 350,
-                height: 31,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 16.0),
-                      child: Text(
-                        '난이도',
-                        style: FarmusThemeTextStyle.gray1Medium15,
-                      ),
+            child: SizedBox(
+              height: 31,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      '난이도',
+                      style: FarmusThemeTextStyle.gray1Medium15,
                     ),
-                    const SizedBox(width: 13),
-                    Container(
-                      child: const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(width: 10),
-                          SearchDifficultyBtn("초급"),
-                          SizedBox(width: 10),
-                          SearchDifficultyBtn("중급"),
-                          SizedBox(width: 10),
-                          SearchDifficultyBtn("고급"),
-                        ],
-                      ),
+                  ),
+                  const SizedBox(width: 13),
+                  Container(
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 10),
+                        SearchDifficultyBtn("초급"),
+                        SizedBox(width: 10),
+                        SearchDifficultyBtn("중급"),
+                        SizedBox(width: 10),
+                        SearchDifficultyBtn("고급"),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
