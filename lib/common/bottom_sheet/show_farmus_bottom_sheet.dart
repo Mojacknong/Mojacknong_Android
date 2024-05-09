b12f5d64796ff78353ac9aa4011bb81d6ce56787
arinming <jyandysys1@naver.com>
2024-05-09 23:51:02 +0900

    [Feat] 루틴 수정시 초기값 반영 (#152)

---
+++ lib/common/bottom_sheet/show_farmus_bottom_sheet.dart
@@ -0,0 +1,30 @@
+import 'package:farmus/view/routine/component/vege_routine_bottom_sheet_content.dart';
+import 'package:flutter/material.dart';
+
+import '../theme/farmus_theme_color.dart';
+
+void showRoutineAddBottomSheet(BuildContext context) {
+  showModalBottomSheet<void>(
+    context: context,
+    isScrollControlled: true,
+    backgroundColor: FarmusThemeColor.white,
+    builder: (BuildContext context) {
+      return const VegeRoutineBottomSheetContent();
+    },
+  );
+}
+
+void showRoutineEditBottomSheet(
+    BuildContext context, String routine, String day,) {
+  showModalBottomSheet<void>(
+    context: context,
+    isScrollControlled: true,
+    backgroundColor: FarmusThemeColor.white,
+    builder: (BuildContext context) {
+      return VegeRoutineBottomSheetContent(
+        routine: routine,
+        day: day,
+      );
+    },
+  );
+}
