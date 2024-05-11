1ccd62216260a3ec19a57e082e8134bfd2d068e3
geonheey <ghyjas0704@gmail.com>
2024-05-12 01:41:32 +0900

    [Feat] SearchScreen앱 바에 있는 검색 기능 준비 (#157)

--- lib/view/search/search_screen.dart
+++ lib/view/search/search_screen.dart
@@ -9,6 +9,7 @@ import 'package:farmus/view/search/component/search_difficulty_box.dart';
 import 'package:farmus/view/search/component/search_farmclub_info.dart';
 import 'package:farmus/view/search/component/search_tab_bar.dart';
 import 'package:farmus/view/search/component/search_welcome_text.dart';
+import 'package:farmus/view/search_farmclub/search_farmclub_screen.dart';
 import 'package:farmus/view_model/search/search_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -45,7 +46,13 @@ class _SearchScreenState extends ConsumerState<SearchScreen> {
             icon: SvgPicture.asset(
               'assets/image/ic_search_glass.svg',
             ),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const SearchFarmclubSceen()),
+              );
+            },
           ),
         ],
       ),
---
+++ lib/view/search_farmclub/search_farmclub_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchFarmclubSceen extends StatefulWidget {
+  const SearchFarmclubSceen({super.key});
+
+  @override
+  State<SearchFarmclubSceen> createState() => _SearchFarmclubScreenState();
+}
+
+class _SearchFarmclubScreenState extends State<SearchFarmclubSceen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
