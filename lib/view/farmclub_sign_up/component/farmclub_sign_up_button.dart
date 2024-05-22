f5f4a24ff2a63262d160a672bcbfd5b865751aa3
geonheey <ghyjas0704@gmail.com>
2024-05-22 23:35:33 +0900

    [Refactor] PrimaryColorButton -> PrimaryButton으로 변경 (#166)

--- lib/view/farmclub_sign_up/component/farmclub_sign_up_button.dart
+++ lib/view/farmclub_sign_up/component/farmclub_sign_up_button.dart
@@ -1,4 +1,6 @@
-import 'package:farmus/common/button/primary_color_button.dart';
+import 'package:farmus/common/button/primary_button.dart';
+import 'package:farmus/common/theme/farmus_theme_color.dart';
+import 'package:farmus/common/theme/farmus_theme_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,18 +9,27 @@ class FarmclubSignUpButton extends ConsumerWidget {
     super.key,
     required this.onPressed,
   });
-
   final void Function()? onPressed;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(8, 4, 8, 32.0),
-      child: SizedBox(
-        width: double.infinity,
-        child: PrimaryColorButton(
-          enabled: true,
-          text: "팜클럽 가입하기",
-          onPressed: () {},
+      padding: const EdgeInsets.all(16.0),
+      child: PrimaryButton(
+        height: 50,
+        enabled: true,
+        textColor: FarmusThemeColor.white,
+        backgroundColor: FarmusThemeColor.primary,
+        borderColor: FarmusThemeColor.white,
+        onPressed: onPressed,
+        buttonChild: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "팜클럽 가입하기",
+              style: FarmusThemeTextStyle.whiteSemiBold15,
+            ),
+          ],
         ),
       ),
     );
