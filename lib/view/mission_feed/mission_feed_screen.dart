e6ec3427fb98f136b4705424bbe576bb0a4e0d55
arinming <jyandysys1@naver.com>
2024-05-22 13:26:18 +0900

    [Feat] 미션 피드 스크롤 뷰 구현 (#168)

--- lib/view/mission_feed/mission_feed_screen.dart
+++ lib/view/mission_feed/mission_feed_screen.dart
@@ -1,4 +1,5 @@
 import 'package:farmus/common/app_bar/back_left_title_app_bar.dart';
+import 'package:farmus/view/farmclub/component/farmclub_open_diary.dart';
 import 'package:farmus/view/mission_write/component/mission_step_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,13 +14,20 @@ class MissionFeedScreen extends ConsumerWidget {
         title: '미션 피드',
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: MissionStepInfo(stepNum: 3, isButton: true,),
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              MissionStepInfo(
+                stepNum: 3,
+                isButton: true,
+              ),
+              FarmclubFeed(),
+              FarmclubFeed(),
+              FarmclubFeed(),
+              FarmclubFeed(),
+            ],
+          ),
         ),
       ),
     );
