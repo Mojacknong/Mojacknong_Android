5291147fe1d6c6c00857eee54180d06aff2ce13a
geonheey <ghyjas0704@gmail.com>
2024-06-05 19:07:00 +0900

    [Refactor] 난이도provider @rivpod provider로 변경 (#185)

--- lib/view_model/search/search_detail_provider.dart
+++ lib/view_model/search/search_detail_provider.dart
@@ -1,27 +1,30 @@
 //탐색탭이란
-import 'package:farmus/data/network/search_farmclub_service.dart';
 import 'package:farmus/model/search/search_farmclub_info_model.dart';
+import 'package:farmus/repository/search_farmclub_repository.dart';
 import 'package:farmus/view_model/search/page_notifier.dart';
 import 'package:farmus/view_model/search/search_difficulty_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'search_detail_provider.g.dart';
 
 final aboutSearchPageProvider = StateNotifierProvider<PageNotifier, int>((ref) {
   return PageNotifier();
 });
 
-//난이도 버튼
 final searchDifficultyBtnProvider =
     StateNotifierProvider<SearchDifficultyNotifier, Map<String, bool>>(
   (ref) => SearchDifficultyNotifier(),
 );
 
-//난이도 필터링
-final searchFarmclubDifficultyProvider =
-    FutureProvider<List<SearchFarmclubInfoModel>?>((ref) async {
-  final service = SearchFarmclubService();
+@riverpod
+Future<List<SearchFarmclubInfoModel>?> searchFarmclubDifficulty(
+    SearchFarmclubDifficultyRef ref) async {
+  final repository = SearchFarmclubRepository();
   final filteredDifficulties =
       ref.read(searchDifficultyBtnProvider.notifier).getFilteredDifficulties();
-  final farmclubs =
-      await service.fetchFarmclubsByDifficulty(filteredDifficulties);
+  final farmclubs = await repository.fetchFarmclubsByDifficulty(
+    filteredDifficulties,
+  );
   return farmclubs;
-});
+}
---
+++ lib/view_model/search/search_detail_provider.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'search_detail_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$searchFarmclubDifficultyHash() =>
+    r'9d3d5ad2e7215996177ccb8ac8aa41f5ac2c7c9c';
+
+/// See also [searchFarmclubDifficulty].
+@ProviderFor(searchFarmclubDifficulty)
+final searchFarmclubDifficultyProvider =
+    AutoDisposeFutureProvider<List<SearchFarmclubInfoModel>?>.internal(
+  searchFarmclubDifficulty,
+  name: r'searchFarmclubDifficultyProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$searchFarmclubDifficultyHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SearchFarmclubDifficultyRef
+    = AutoDisposeFutureProviderRef<List<SearchFarmclubInfoModel>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
