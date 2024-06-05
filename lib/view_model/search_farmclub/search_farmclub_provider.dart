0a6b499f80d7573b4820768ff7864ed5c2042c8f
geonheey <ghyjas0704@gmail.com>
2024-06-06 00:01:08 +0900

    [Feat] 팜클럽 검색 창 API 연동 (#192)

--- lib/view_model/search_farmclub/search_farmclub_provider.dart
+++ lib/view_model/search_farmclub/search_farmclub_provider.dart
@@ -1,15 +1,20 @@
-import 'package:farmus/data/network/search_farmclub_service.dart';
 import 'package:farmus/model/search/search_farmclub_info_model.dart';
+import 'package:farmus/repository/search_farmclub_repository.dart';
 import 'package:farmus/view_model/search_farmclub/notifier/delete_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-//검색창 텍스트 삭제
-final searchFarmclubTextbarProvider =
+part 'search_farmclub_provider.g.dart';
+
+final searchFarmclubTextbarDeleteProvider =
     StateNotifierProvider<DeletelNotifier, String>((ref) {
   return DeletelNotifier();
 });
 
-//검색
-final searchFarmclubProvider = FutureProvider<List<SearchFarmclubInfoModel>?>(
-  (ref) => SearchFarmclubService().searchFarmclubs(keyword: ''),
-);
+@riverpod
+Future<List<SearchFarmclubInfoModel>?> searchFarmclubsResult(
+    SearchFarmclubsResultRef ref, String keyword) async {
+  final repository = SearchFarmclubRepository();
+  final response = await repository.fetchFarmclubs(keyword);
+  return response;
+}
