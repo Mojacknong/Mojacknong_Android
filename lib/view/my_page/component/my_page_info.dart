7e608b5783597af0c8e350b408fb68594470d528
geonheey <ghyjas0704@gmail.com>
2024-08-03 03:31:12 +0900

    [Chore] unused import 삭제 (#222)

--- lib/view/my_page/component/my_page_info.dart
+++ lib/view/my_page/component/my_page_info.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'package:farmus/common/theme/farmus_theme_color.dart';
 import 'package:farmus/common/theme/farmus_theme_text_style.dart';
-import 'package:farmus/model/my_page/my_page_info_model.dart';
 import 'package:farmus/view/my_page/my_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
