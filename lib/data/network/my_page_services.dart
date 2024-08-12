dad828616ab90d29398855d1600fa77235393bb2
geonheey <ghyjas0704@gmail.com>
2024-08-12 16:38:35 +0900

    [Feat] "프로필 닉네임만" 수정 서비스 추가 (#234)

--- lib/data/network/my_page_services.dart
+++ lib/data/network/my_page_services.dart
@@ -8,24 +8,18 @@ class MyPageServices {
     const url = '/api/user/profile';
 
     ApiClient apiClient = ApiClient();
-    if (!await file.exists()) {
-      file = File("");
-    }
+
     final response = await apiClient.postMultipart(
       url,
       nickname,
       file,
     );
 
-    print('서비스 쪽 : ${utf8.decode(response.bodyBytes)}');
-
     if (response.statusCode == 200) {
       return utf8.decode(response.bodyBytes);
-
     } else {
       throw Exception('Failed to post profile');
     }
-
   }
 
   Future<String> myInfo() async {
@@ -39,6 +33,25 @@ class MyPageServices {
       return utf8.decode(response.bodyBytes);
     } else {
       throw Exception('Failed to User Info');
+    }
+  }
+
+  Future<String> postNickname(String nickname) async {
+    const url = '/api/user/nickname';
+
+    final body = jsonEncode({
+      'nickname': nickname,
+    });
+
+    ApiClient apiClient = ApiClient();
+
+    final response = await apiClient
+        .patch(url, body: body, headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 200) {
+      return utf8.decode(response.bodyBytes);
+    } else {
+      throw Exception('닉네임 수정 실패: ${response.statusCode} ${response.body}');
     }
   }
 }
