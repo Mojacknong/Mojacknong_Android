bcdb69ce6c2ac6b75bdcd0c4ae423c6cdbc96d85
geonheey <ghyjas0704@gmail.com>
2024-08-15 16:53:03 +0900

    [Feat] text input 온보딩과 프로필 수정 분리 (#227)

--- lib/view/on_boarding/component/on_boarding_nickname_text_input.dart
+++ lib/view/on_boarding/component/on_boarding_nickname_text_input.dart
@@ -5,60 +5,41 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../common/theme/farmus_theme_color.dart';
 import '../../../view_model/on_boarding/on_boarding_provider.dart';
 
-class OnBoardingNicknameTextInput extends ConsumerStatefulWidget {
-  const OnBoardingNicknameTextInput({
-    super.key,
-    this.initialValue,
-    this.errorText,
-  });
+class OnBoardingNicknameTextInput extends ConsumerWidget {
+  const OnBoardingNicknameTextInput(
+      {super.key, this.initialValue, this.errorText});
 
   final String? initialValue;
   final String? errorText;
 
   @override
-  ConsumerState<OnBoardingNicknameTextInput> createState() => _OnBoardingNicknameTextInputState();
-}
-
-class _OnBoardingNicknameTextInputState extends ConsumerState<OnBoardingNicknameTextInput> {
-  late TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController(text: widget.initialValue);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final nowLength = _controller.text.length;
+  Widget build(BuildContext context, WidgetRef ref) {
+    var nowLength = ref.watch(onBoardingProfileSetProvider).nickname == null
+        ? 0
+        : ref.watch(onBoardingProfileSetProvider).nickname?.length;
 
     return PrimaryTextFormField(
-      controller: _controller,
-      maxLength: 10,
-      maxLines: 1,
-      minLines: 1,
-      hintText: '파머',
-      errorText: widget.errorText,
-      errorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: const BorderSide(
-          color: FarmusThemeColor.gray4,
+        initialValue: initialValue,
+        maxLength: 10,
+        maxLines: 1,
+        minLines: 1,
+        hintText: '파머',
+        errorText: errorText,
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: const BorderSide(
+            color: FarmusThemeColor.gray4,
+          ),
         ),
-      ),
-      errorStyle: const TextStyle(
-        color: FarmusThemeColor.red,
-      ),
-      onChanged: (value) {
-        ref.read(onBoardingProfileSetProvider.notifier).updateNickname(value);
-        ref.read(onBoardingSpecialCharactersProvider.notifier).checkForSpecialCharacters(value);
-      },
-      suffix: Text("$nowLength /10"),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+        errorStyle: const TextStyle(
+          color: FarmusThemeColor.red,
+        ),
+        onChanged: (value) {
+          ref.read(onBoardingProfileSetProvider.notifier).updateNickname(value);
+          ref
+              .read(onBoardingSpecialCharactersProvider.notifier)
+              .checkForSpecialCharacters(value);
+        },
+        suffix: Text("$nowLength /10"));
   }
 }
