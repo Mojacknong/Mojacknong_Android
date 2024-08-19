4f133e3730a696941e10fcf69b10823d5aac3258
geonheey <ghyjas0704@gmail.com>
2024-08-19 17:07:27 +0900

    [Feat] 탐색탭 "파머"이름 API 연동 (#246)

--- lib/view/search/component/search_welcome_text.dart
+++ lib/view/search/component/search_welcome_text.dart
@@ -1,29 +1,42 @@
-import 'package:farmus/common/theme/farmus_theme_text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:farmus/common/theme/farmus_theme_text_style.dart';
 
-class SearchWelcomeText extends StatelessWidget {
-  const SearchWelcomeText({Key? key}) : super(key: key);
+import '../../../view_model/my_page/my_page_info_provider.dart';
+
+class SearchWelcomeText extends ConsumerWidget {
+  const SearchWelcomeText({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      child: const Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '파머 님을 위한\n추천 팜클럽',
-            style: FarmusThemeTextStyle.darkSemiBold20,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final myPageInfoAsyncValue = ref.watch(myPageInfoModelProvider);
+
+    return myPageInfoAsyncValue.when(
+      data: (myPageInfo) {
+        final nickName = myPageInfo.nickName;
+
+        return Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '$nickName님을 위한\n추천 팜클럽',
+                style: FarmusThemeTextStyle.darkSemiBold20,
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                '홈파밍 초보도 도전할 수 있는 채소예요.',
+                style: FarmusThemeTextStyle.gray2Medium15,
+              ),
+            ],
           ),
-          SizedBox(height: 8),
-          Text(
-            '홈파밍 초보도 도전할 수 있는 채소예요.',
-            style: FarmusThemeTextStyle.gray2Medium15,
-          ),
-        ],
-      ),
+        );
+      },
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stackTrace) => const Center(child: Text('Something went wrong')),
     );
   }
 }
