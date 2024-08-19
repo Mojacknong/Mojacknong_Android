7e7629002feec9c93f3be4a9b53b0f1845f100a4
arinming <jyandysys1@naver.com>
2024-08-20 01:10:59 +0900

    [Feat] 응원 문구 조회 (#230)

--- lib/view/home/component/home_motivation.dart
+++ lib/view/home/component/home_motivation.dart
@@ -20,38 +20,39 @@ class HomeMotivation extends ConsumerWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
-            decoration: ShapeDecoration(
-              color: const Color(0xFFE0F490),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24),
-              ),
-            ),
-            child: motivation.when(
-              data: (data) {
-                return const Row(
+      child: motivation.when(
+        data: (data) {
+          return Column(
+            children: [
+              Container(
+                width: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFE0F490),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
                       child: Text(
-                        '텃밭에서 식탁까지 팜어스와 늘 함께해요!텃밭에서 식탁까지 팜어스와 늘 함께해요!',
+                        data.encouragementMessage,
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
                         style: FarmusThemeTextStyle.darkMedium13,
                       ),
                     ),
                   ],
-                );
-              },
-              loading: () => const CircularProgressIndicator(),
-              error: (error, stack) => Text('Error: $error'),
-            ),
-          ),
-        ],
+                ),
+              ),
+            ],
+          );
+        },
+        loading: () => const CircularProgressIndicator(),
+        error: (error, stack) => Text('Error: $error'),
       ),
     );
   }
