a006ce602468b80512844ef12c3f5fde25f2db53
geonheey <ghyjas0704@gmail.com>
2024-08-21 15:00:52 +0900

    [Feat] 팜클럽 미션 완료 모델 생성 및 notifier생성 (#250)

---
+++ lib/view_model/mission_write/notifier/post_mission_notifier.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+import 'package:farmus/model/mission/mission_write_model.dart';
+import 'package:farmus/repository/my_farmclub_repository.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'post_mission_notifier.g.dart';
+
+@riverpod
+class PostMissionNotifier extends _$PostMissionNotifier {
+  @override
+  Future<MissionWriteModel> build() async {
+    return MissionWriteModel(
+      file: null,
+      content: '',
+      farmClubId: -1,
+      isComplete: false,
+    );
+  }
+
+  bool _isMissionComplete = false;
+
+  bool get isMissionComplete => _isMissionComplete;
+
+  void updateImage(XFile? image) {
+    state = AsyncData(
+      MissionWriteModel(
+        file: image,
+        content: state.value!.content,
+        farmClubId: state.value!.farmClubId,
+      ),
+    );
+    _checkCompletion();
+  }
+
+  void updateContent(String? content) {
+    if (content == null) {
+      return;
+    }
+    state = AsyncData(
+      MissionWriteModel(
+        file: state.value!.file,
+        content: content,
+        farmClubId: state.value!.farmClubId,
+      ),
+    );
+    _checkCompletion();
+  }
+
+  void deleteImage() {
+    state = AsyncData(
+      MissionWriteModel(
+        file: null,
+        content: state.value!.content,
+        farmClubId: state.value!.farmClubId,
+      ),
+    );
+    _checkCompletion();
+  }
+
+  void _checkCompletion() {
+    if (state.value?.content.isNotEmpty == true &&
+            state.value?.file?.path.isNotEmpty == true
+        // && state.value?.farmClubId.isNotEmpty == true
+        ) {
+      _isMissionComplete = true;
+    } else {
+      _isMissionComplete = false;
+    }
+    state = AsyncData(
+      MissionWriteModel(
+        file: state.value!.file,
+        content: state.value!.content,
+        farmClubId: state.value!.farmClubId,
+        isComplete: _isMissionComplete,
+      ),
+    );
+  }
+
+  Future<void> postMission(File image, String content, int farmClubId) async {
+    await MyFarmclubRepository.postMission(image, content, farmClubId);
+    ref.invalidateSelf();
+  }
+}
---
+++ lib/view_model/mission_write/notifier/post_mission_notifier.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_mission_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$postMissionNotifierHash() =>
+    r'feb8d7c7ea9821191a6a4a40978d8cf3b8257cec';
+
+/// See also [PostMissionNotifier].
+@ProviderFor(PostMissionNotifier)
+final postMissionNotifierProvider = AutoDisposeAsyncNotifierProvider<
+    PostMissionNotifier, MissionWriteModel>.internal(
+  PostMissionNotifier.new,
+  name: r'postMissionNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$postMissionNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PostMissionNotifier = AutoDisposeAsyncNotifier<MissionWriteModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
