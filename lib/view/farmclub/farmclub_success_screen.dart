1ec7d1e4de8eade988f2b67928ccc092705cec81
geonheey <ghyjas0704@gmail.com>
2024-08-23 19:08:27 +0900

    [Feat] 팜클럽 성공 API 연동 완료 및 전체 화면 (#257)

--- lib/view/farmclub/farmclub_success_screen.dart
+++ lib/view/farmclub/farmclub_success_screen.dart
@@ -1,45 +1,97 @@
 import 'package:farmus/common/app_bar/primary_app_bar.dart';
-import 'package:farmus/view/home/home_screen.dart'; // 이동할 페이지의 import 추가
+import 'package:farmus/common/theme/farmus_theme_text_style.dart';
+import 'package:farmus/view_model/home/home_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../common/button/primary_color_button.dart';
+import '../../model/my_farmclub/farmclub_success_model.dart';
+import '../../view_model/my_farmclub/farmclub_success_info_notifier.dart';
 import '../main/main_screen.dart';
+import 'component/farmclub_success_info.dart';
 
 class FarmclubSuccessScreen extends ConsumerWidget {
   const FarmclubSuccessScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var selectedFarmclubId = ref.watch(selectedFarmclubIdProvider);
+    final AsyncValue<FarmclubSuccessModel> myFarmclubSuccessList =
+        ref.watch(farmclubSuccessModelProvider(selectedFarmclubId));
+
     return Scaffold(
-      appBar: PrimaryAppBar(),
+      appBar: PrimaryAppBar(
+        leading: Container(),
+        title: null,
+      ),
       body: Column(
         children: [
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(16.0),
               child: Center(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // Align content to start
                   children: [
                     const Text(
-                      '미션이 성공적으로 완료되었습니다!',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      '축하해요\n팜클럽을 끝까지 완주했어요!',
+                      style: FarmusThemeTextStyle.darkSemiBold20,
+                      textAlign: TextAlign.start, // Align text to the start
                     ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MainScreen(selectedIndex: 0,),
+                    const Text(
+                      '앞으로의 홈파밍 여정도 팜어스와 함께해요!',
+                      style: FarmusThemeTextStyle.gray2Medium14,
+                      textAlign: TextAlign.start, // Align text to the start
+                    ),
+                    myFarmclubSuccessList.when(
+                      data: (data) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 40.0),
+                          child: FarmclubSuccessInfo(
+                            myFarmclubSuccessList: data,
+                            selectedFarmclubId: selectedFarmclubId,
                           ),
                         );
                       },
-                      child: const Text('홈으로 가기'),
+                      loading: () => const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                      error: (error, stack) => Center(
+                        child: Text('Error: $error'),
+                      ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: PrimaryColorButton(
+                        enabled: true,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MainScreen(
+                                selectedIndex: 0,
+                              ),
+                            ),
+                          );
+                        },
+                        buttonChild: const Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(width: 8),
+                              Text(
+                                "팜클럽 마치기",
+                                style: FarmusThemeTextStyle.whiteSemiBold15,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
