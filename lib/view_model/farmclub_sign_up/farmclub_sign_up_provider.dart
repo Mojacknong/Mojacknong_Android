2a13004ede4f7e8bc308b5f6eef486a53a53479e
geonheey <ghyjas0704@gmail.com>
2024-09-23 12:41:11 +0900

    [Feat] 토글 버튼 초기화 (#278)

--- lib/view_model/farmclub_sign_up/farmclub_sign_up_provider.dart
+++ lib/view_model/farmclub_sign_up/farmclub_sign_up_provider.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:farmus/model/search/search_farmclub_detail_model.dart';
 import 'package:farmus/repository/search_farmclub_repository.dart';
+import 'package:farmus/view_model/farmclub_sign_up/toggle_notifier_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'farmclub_sign_up_provider.g.dart';
@@ -9,7 +10,10 @@ part 'farmclub_sign_up_provider.g.dart';
 @riverpod
 Future<SearchFarmclubDetailModel> searchFarmclubDetailModel(
     SearchFarmclubDetailModelRef ref, int id) async {
+  ref.invalidate(toggleNotifierProvider);
+
   final repository = SearchFarmclubRepository();
   final response = await repository.farmclubDetail(id);
   return response;
+
 }
